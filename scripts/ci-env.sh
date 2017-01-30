d5aac7f03b85ea57ac701777ed77a92554c5cee1
Joe Black <joeblack949@gmail.com>
2017-01-30 06:52:56 -0500

    Added CI

---
+++ scripts/ci-env.sh
@@ -0,0 +1,14 @@
+# Creates a stable branch identifier when executing CI on pull requests.
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+    export BRANCH=$TRAVIS_BRANCH
+else
+    export BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+fi
+
+export REPO=$DOCKER_USER/$(basename $TRAVIS_REPO_SLUG)
+
+if [[ $BRANCH == "master" ]]; then
+    export TAG='latest'
+else
+    export TAG=$BRANCH
+fi
