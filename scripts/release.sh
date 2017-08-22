885dae32572be53e64a5b2e6243f67ba6f1059a1
Joe Black <joeblackwaslike@me.com>
2017-08-22 01:35:44 -0400

    Tag for release: 0.2 ...

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 REMOTE=${1:-origin}
 
-CURRENT_TAG=$(git tag | sort -n | tail -1)
+CURRENT_TAG=$(git tag | sort -n | tail -1 | sed 's/^v//')
 
 CURRENT_MAJOR=$(echo $CURRENT_TAG | cut -d'.' -f1)
 CURRENT_MINOR=$(echo $CURRENT_TAG | cut -d'.' -f2)
@@ -13,10 +13,10 @@ NEXT_TAG="${CURRENT_MAJOR}.${CURRENT_MINOR}"
 sed -i "/^version/s/$CURRENT_TAG/$NEXT_TAG/g" templates/vars.yaml
 inv templates
 git add .
-git commit -m "Increment for tag $NEXT_TAG ..."
+git commit -m "Tag for release: $NEXT_TAG ..."
 
 echo "Incrementing version $CURRENT_TAG > $NEXT_TAG ..."
-git tag $NEXT_TAG
+git tag v$NEXT_TAG
 
 echo "Pushing to github remote: $REMOTE ..."
-git push $REMOTE master --tags
+# git push $REMOTE master --tags
